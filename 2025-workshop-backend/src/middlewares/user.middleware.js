31615cd1c711ceceb660eda11e2290bdca10b136
PDJ6975 <antrodcal@alum.us.es>
2025-12-27 11:51:10 +0100

    feat: añadir validación máxima middleware

--- 2025-workshop-backend/src/middlewares/user.middleware.js
+++ 2025-workshop-backend/src/middlewares/user.middleware.js
@@ -1,22 +1,21 @@
 import { body, validationResult } from 'express-validator';
 
 export const validateCreateAndUpdateUser = [
-    // Validate 'name' field
-    body('name')
-        .exists({ checkNull: true })
-        .withMessage('Name is required')
-        .isString()
-        .withMessage('Name must be a string')
-        .isLength({ min: 3 })
-        .withMessage('Name must be at least 3 characters long'),
-        //TODO: Añadir máximo de 50 caracteres.
+  // Validate 'name' field
+  body('name')
+    .exists({ checkNull: true })
+    .withMessage('Name is required')
+    .isString()
+    .withMessage('Name must be a string')
+    .isLength({ min: 3, max: 50 })
+    .withMessage('Name must be between 3 and 50 characters long'),
 
-    // Middleware to handle validation errors
-    (req, res, next) => {
-        const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-        next();
-    },
+  // Middleware to handle validation errors
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    next();
+  },
 ];
