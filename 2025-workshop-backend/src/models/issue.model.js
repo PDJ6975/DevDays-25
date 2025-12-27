6ef27991d313c396b0a01d646bce400d315fc462
PDJ6975 <antrodcal@alum.us.es>
2025-12-27 12:03:48 +0100

    feat: añadir campo updated_at para las issues

--- 2025-workshop-backend/src/models/issue.model.js
+++ 2025-workshop-backend/src/models/issue.model.js
@@ -1,36 +1,39 @@
 import mongoose from 'mongoose';
 
 const issueSchema = new mongoose.Schema({
-    issueId: {
-        type: Number,
-        required: true,
-        unique: true,
-    },
-    number: {
-        type: Number,
-        required: true,
-    },
-    url: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    createdAt: {
-        type: Date,
-        required: true,
-    },
-    // TODO: Store the updatedAt field from the GitHub issue
-    title: {
-        type: String,
-        required: true,
-    },
-    body: {
-        type: String,
-        required: false,
-    },
+	issueId: {
+		type: Number,
+		required: true,
+		unique: true,
+	},
+	number: {
+		type: Number,
+		required: true,
+	},
+	url: {
+		type: String,
+		required: true,
+	},
+	state: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		required: true,
+	},
+	updatedAt: {
+		type: Date,
+		required: true,
+	},
+	title: {
+		type: String,
+		required: true,
+	},
+	body: {
+		type: String,
+		required: false,
+	},
 });
 
 const Issue = mongoose.model('Issue', issueSchema);
